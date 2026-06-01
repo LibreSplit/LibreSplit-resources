89aa00e600bf2a2a297a1ba5eb06827807be4188
loldonut <loldonutph@gmail.com>
2026-06-01 17:22:24 +0800

    fix campaign splitting

--- auto-splitters/L4D2/README.md
+++ auto-splitters/L4D2/README.md
@@ -3,10 +3,8 @@
 A Left 4 Dead 2 Autosplitter specifically for the v2.0.0.0 version of the game.
 
 - This autosplitter is not complete and lacking some features from the original autosplitter from LiveSplit
-- Individual Level runs should work fine.
-- Main Campaign runs currently have splitting issues when used with a vote cycler
+- Individual Level and Main Campaign runs should work fine.
 
 ## TODO
 
-- Add multiple versions
-- Proper campaign split
+- Add "burhac" option (split after intro cutscene)
--- auto-splitters/L4D2/l4d2.lua
+++ auto-splitters/L4D2/l4d2.lua
@@ -2,30 +2,54 @@
 -- Ported from the L4D2 LiveSplit ASL (WIP)
 process("left4dead2.exe")
 
+local settings = {
+    -- DEFAULT : TRUE
+    chapterSplit = true,
+    scoreboardVSgameLoading = true
+}
+
 local current = {
+    whatsLoading = "",
     isLoading = false,
     hasControl = false,
     scoreboardLoading = false,
-    svCheats = false
+    svCheats = false,
+    cutscenePlaying = false
 }
 
 local old = {
+    whatsLoading = "",
     isLoading = false,
     hasControl = false,
     scoreboardLoading = false,
-    svCheats = false
+    svCheats = false,
+    cutscenePlaying = false
 }
 
 local loadCount = 0
 
 -- VERSION 2000 OFFSETS
 local offsets = {
-    wl = 0x3C9988,
-    gl = 0x5CC89C,
-    hc = 0x68FBD4,
-    sl = 0x6DB58D,
-    ft = 0x6ED414,
-    sv = 0x6DB040
+    whatsLoading = 0x3C9988,
+    gameLoading = 0x5CC89C,
+    hasControl = 0x68FBD4,
+    scoreboardLoad = 0x6DB58D,
+    finaleTrigger = 0x6ED414,
+    svCheats = 0x6DB040,
+    cutscenePlaying = 0x66CEEC
+}
+
+-- More maps should be added here when more versions are supported
+-- in the near future
+local campaignsLastMaps = {
+    c5m5_bridge = true
+}
+local campaignsFirstMaps = {
+    c1m1_hotel        = true,
+    c2m1_highway      = true,
+    c3m1_plankcountry = true,
+    c4m1_milltown_a   = true,
+    c5m1_waterfront   = true
 }
 
 local cutsceneStart = nil
@@ -38,7 +62,7 @@ local function ticksToMs(t) return t * (1000 / ticksPerSec) end
 local function msToTicks(ms) return math.ceil(ms * ticksPerSec / 1000) end
 local cutsceneMinTicks = msToTicks(250)
 
-function cutsceneElapsed()
+local function cutsceneElapsed()
     if cutsceneElapsed == nil then return 0 end
     return tickCount - cutsceneStart
 end
@@ -63,18 +87,22 @@ end
 function state()
     tickCount = tickCount + 1
     old = shallow_copy_tbl(current)
-    current.isLoading = readAddress("bool", "engine.dll", offsets.gl)
-    current.hasControl = readAddress("bool", "Client.dll", offsets.hc)
-    current.scoreboardLoading = readAddress("bool", "Client.dll", offsets.sl)
-    current.finaleTrigger = readAddress("bool", "Client.dll", offsets.ft)
-    current.svCheats = readAddress("bool", "Client.dll", offsets.sv, 0x30)
+    current.whatsLoading = readAddress("string30", "engine.dll", offsets.whatsLoading)
+    current.isLoading = readAddress("bool", "engine.dll", offsets.gameLoading)
+    current.hasControl = readAddress("bool", "Client.dll", offsets.hasControl)
+    current.scoreboardLoading = readAddress("bool", "Client.dll", offsets.scoreboardLoad)
+    current.finaleTrigger = readAddress("bool", "Client.dll", offsets.finaleTrigger)
+    current.svCheats = readAddress("bool", "Client.dll", offsets.svCheats, 0x30)
+    current.cutscenePlaying = readAddress("bool", "Client.dll", offsets.cutscenePlaying)
 
-    print("map: " .. readAddress("string30", "engine.dll", offsets.wl))
-    print("hc: " .. tostring(current.hasControl))
+    print("map: " .. current.whatsLoading)
+    print("hasControl: " .. tostring(current.hasControl))
     print("game loading: " .. tostring(current.isLoading))
-    print("sl: " .. tostring(current.scoreboardLoading))
+    print("scoreboardLoad: " .. tostring(current.scoreboardLoading))
     print("sv_cheats: " .. tostring(current.svCheats))
     print("finaleTrigger: " .. tostring(current.finaleTrigger))
+    print("cutscenePlaying: " .. tostring(current.cutscenePlaying))
+    print("lastSplit: " .. lastSplit)
     print()
 end
 
@@ -106,6 +134,7 @@ function start()
 end
 
 function split()
+    -- This relies on having Game Instructor enabled
     if current.finaleTrigger and not old.finaleTrigger then
         delayedReset()
         -- prevent double split
@@ -115,6 +144,11 @@ function split()
         print("finale split")
         lastSplit = current.whatsLoading
         return true
+    elseif current.cutscenePlaying and not old.cutscenePlaying and campaignsLastMaps[current.whatsLoading] then
+        delayedStart()
+        if current.whatsLoading == lastSplit then
+            return false
+        end
     end
 
     if delayedElapsedMs() >= 200 then
@@ -123,9 +157,21 @@ function split()
         return true
     end
 
-    if not current.finaleTrigger and not old.scoreboardLoading and current.scoreboardLoading then
-        lastSplit = current.whatsLoading
-        return true
+    if settings.chapterSplit then
+        if settings.scoreboardVSgameLoading then
+            if not current.finaleTrigger
+                and not old.scoreboardLoading and current.scoreboardLoading then
+                lastSplit = current.whatsLoading
+                return true
+            end
+        else
+            if not current.finaleTrigger
+                and not old.isLoading and current.isLoading
+                and current.scoreboardLoading then
+                lastSplit = current.whatsLoading
+                return true
+            end
+        end
     end
 end
 
