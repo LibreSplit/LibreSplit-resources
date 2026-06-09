23d79bbce9c19cff7c84f73ad6c495d731e8e303
John Heinrich <loldonutph@gmail.com>
2026-06-10 00:36:11 +0800

    add version detection using md5sum

--- auto-splitters/L4D2/l4d2.lua
+++ auto-splitters/L4D2/l4d2.lua
@@ -1,5 +1,5 @@
--- Left 4 Dead 2 autosplitter for V2000 version
--- Ported from the L4D2 LiveSplit ASL (WIP)
+-- Left 4 Dead 2 autosplitter
+-- Ported from the L4D2 LiveSplit ASL
 cmdline("left4dead2.exe")
 
 local settings = {
@@ -8,9 +8,9 @@ local settings = {
     scoreboardVSgameLoading = true
 }
 
-local engineAndClientSizesAndOffsets = {
+local engineAndClientHashesAndOffsets = {
     -- V2000 offsets
-    ["335872_4837376"] = {
+    ["6CBADAA9132AD6138F3D50920BB9ECFE_B23B95981DE30C91C8C9C3E3FD7F3E84"] = {
         whatsLoading = 0x3C9988,
         gameLoading = 0x5CC89C,
         hasControl = 0x68FBD4,
@@ -20,7 +20,7 @@ local engineAndClientSizesAndOffsets = {
         cutscenePlaying = 0x66CEEC
     },
     -- V2012 offsets
-    ["339968_4825088"] = {
+    ["F79518A744FE34538726AAC4A9E43593_474DB57CBCDC9819AA36FBFA55CCFBF5"] = {
         whatsLoading = 0x3CD988,
         gameLoading = 0x5D091C,
         hasControl = 0x699164,
@@ -29,8 +29,18 @@ local engineAndClientSizesAndOffsets = {
         svCheats = 0x6E4738,
         cutscenePlaying = 0x67647C
     },
+    -- V2027 offsets
+    ["6BD61059254B425464507486F096D209_263E9AE9ABA9C751A14663DF432EE9EA"] = {
+        whatsLoading = 0x3CF988,
+        gameLoading = 0x5D291C,
+        hasControl = 0x699264,
+        scoreboardLoad = 0x6E4D6D,
+        finaleTrigger = 0x6F6BF4,
+        svCheats = 0x6E4820,
+        cutscenePlaying = 0x676584
+    },
     -- V2045 offsets
-    ["348160_4771840"] = {
+    ["9B767FB9EC1AA2C35401F1A7310B0943_281AE29C235AACDEC83EE7471175D390"] = {
         gameLoading = 0x5DE494,
         whatsLoading = 0x3D2A00,
         hasControl = 0x6A9C64,
@@ -39,6 +49,46 @@ local engineAndClientSizesAndOffsets = {
         svCheats = 0x6F5270,
         cutscenePlaying = 0x686F7C
     },
+    -- V2075 offsets
+    ["C7BA3CF5AC8722BCA7CBCA0BFB4BCB5B_352E7987A4D778EEA082D2CCB8967EEB"] = {
+        gameLoading = 0x5DA8CC,
+        whatsLoading = 0x3CF630,
+        hasControl = 0x6ABAC4,
+        scoreboardLoad = 0x6F761D,
+        finaleTrigger = 0x709634,
+        svCheats = 0x6F70D0,
+        cutscenePlaying = 0x688E14
+    },
+    -- V2091 offsets
+    ["074A3EF53F97661C724C5FD5FE7F33D8_9A88102D7D7D7D55A2DCBF67406378F7"] = {
+        whatsLoading = 0x3CF630,
+        gameLoading = 0x5E19D4,
+        hasControl = 0x6ABB24,
+        scoreboardLoad = 0x6F7685,
+        finaleTrigger = 0x7096AC,
+        svCheats = 0x6F7138,
+        cutscenePlaying = 0x688E64
+    },
+    -- V2147 offsets
+    ["7899154C8A5263F8919A8E272E1C65AD_D75DD50CBB1A8B9F6106B7B38A5293F7"] = {
+        whatsLoading = 0x42F240,
+        gameLoading = 0x46C54C,
+        hasControl = 0x72767C,
+        scoreboardLoad = 0x775AB5,
+        finaleTrigger = 0x787E14,
+        svCheats = 0x775568,
+        cutscenePlaying = 0x702C64
+    },
+    -- V2203 offsets
+    ["D6DCB5F35F8CA379E1649AE5E5BD0B52_1339CD4EF916923DA04B04904C1B544C"] = {
+        whatsLoading = 0x435240,
+        gameLoading = 0x47264C,
+        hasControl = 0x73421C,
+        scoreboardLoad = 0x782E55,
+        finaleTrigger = 0x7951D4,
+        svCheats = 0x782908,
+        cutscenePlaying = 0x70F804
+    }
 }
 
 local current = {
@@ -59,18 +109,39 @@ local old = {
     cutscenePlaying = false
 }
 
+function string:endsWith(suffix)
+    return self:sub(-#suffix) == suffix
+end
+
+local function getEngineClientMaps()
+    local maps = getMaps()
+    local output = {}
+    for _, map in ipairs(maps) do
+        if map.name:endsWith("engine.dll") then
+            output['engineBase'] = map
+        elseif map.name:endsWith("Client.dll") then
+            output['clientBase'] = map
+        end
+    end
+
+    return output
+end
+
 local function getOffsets()
-    local engineSize = getModuleSize("engine.dll")
-    local clientSize = getModuleSize("Client.dll")
+    local gameMaps = getEngineClientMaps()
 
-    -- UNCOMMENT WHEN DEBUGGING
-    print("Engine Size: " .. tostring(getModuleSize("engine.dll")))
-    print("Client Size: " .. tostring(getModuleSize("Client.dll")))
+    if gameMaps.engineBase ~= nil or gameMaps.clientBase ~= nil then
+        local engineHash = md5sum(gameMaps.engineBase.name):upper()
+        local clientHash = md5sum(gameMaps.clientBase.name):upper()
 
-    local key = string.format("%s_%s", engineSize, clientSize)
+        local key = string.format("%s_%s", engineHash, clientHash)
 
-    if engineSize ~= nil or clientSize ~= nil then
-        return engineAndClientSizesAndOffsets[key]
+        -- UNCOMMENT WHEN DEBUGGING
+        -- print("Engine Hash : " .. tostring(engineHash))
+        -- print("Client Hash : " .. tostring(clientHash))
+        -- print("OFFSETS KEY : " .. key)
+
+        return engineAndClientHashesAndOffsets[key]
     else
         return nil
     end
